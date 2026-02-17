b3d8c23b9a092eb4ffe2f1b8cfc544e7ff8c84fe
Preet Taparia <preettaparia@jklu.edu.in>
2026-02-17 14:28:51 +0530

    update cors

--- backend/app.js
+++ backend/app.js
@@ -22,11 +22,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET",
-    "POST",
-    "PATCH",
-    "DELETE",
-    "OPTIONS"
+    "GET,POST,PATCH,DELETE,OPTIONS",
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -78,4 +74,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
